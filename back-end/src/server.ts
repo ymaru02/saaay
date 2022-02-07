aa50868551d4a4a7f3885dc0c9ec2f8cd71743b3
김석원 <seokworking1609@gmail.com>
2022-02-07 14:21:29 +0900

    add roomroute in server.ts

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -2,13 +2,14 @@ import express, { Request, Response, NextFunction } from "express";
 import { json } from "body-parser";
 
 import sampleRoutes from "./routes/SampleRoute";
+import roomRoute from "./routes/RoomRoute";
 
 const app = express();
 
 app.use(json());
 
 app.use("/sample", sampleRoutes);
-
+app.use("/room", roomRoute);
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ messager: err.message });
 });
