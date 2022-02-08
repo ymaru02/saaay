92d882cdb82f27effbf8d9358bc7bc2c33feb3a0
이재만 <lbefull@naver.com>
2022-02-08 10:50:55 +0900

    팔로우 페이지 초안

--- front-end/src/pages/account.vue
+++ front-end/src/pages/account.vue
@@ -1,9 +1,48 @@
 <template>
-  <div></div>
+  <div class="q-pa-md">
+    <div class="row">
+      <div class="col-8 offset-2">
+        <q-card>
+          <q-tabs
+            v-model="tab"
+            dense
+            class="text-grey"
+            active-color="primary"
+            indicator-color="primary"
+            align="justify"
+            narrow-indicator
+          >
+            <q-tab name="followers" label="Followers" />
+            <q-tab name="following" label="Following" />
+          </q-tabs>
+
+          <q-separator />
+
+          <q-tab-panels v-model="tab" animated>
+            <q-tab-panel name="followers">
+              <div class="text-h6">Name</div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </q-tab-panel>
+
+            <q-tab-panel name="following">
+              <div class="text-h6">Name</div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </q-tab-panel>
+          </q-tab-panels>
+        </q-card>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
-</script>
+import { ref } from 'vue';
 
-<style></style>
+export default {
+  setup() {
+    return {
+      tab: ref('followers'),
+    };
+  },
+};
+</script>
