ea55c185a85957ca19c8c20673246313bc7dc148
kcw0360 <kcw0360@naver.com>
2022-02-08 15:20:36 +0900

    temp

--- front-end/src/event-utils.ts
+++
@@ -1,8 +0,0 @@
-
-let eventGuid = 0
-// eslint-disable-next-line prefer-const
-// let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
-
-export function createEventId() {
-  return String(eventGuid++)
-}
