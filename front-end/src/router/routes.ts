80a92b8e06aeda76500c61ec8e8d430a4674069f
이재만 <lbefull@naver.com>
2022-02-08 10:25:08 +0900

    account 컴포넌트 생성 및 경로 연결

--- front-end/src/router/routes.ts
+++ front-end/src/router/routes.ts
@@ -7,6 +7,12 @@ const routes: RouteRecordRaw[] = [
     children: [{ path: '', component: () => import('pages/Index.vue') }],
   },
 
+  {
+    path: '/account',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/account.vue') }],
+  },
+
   // Always leave this as last one,
   // but you can also remove it
   {
