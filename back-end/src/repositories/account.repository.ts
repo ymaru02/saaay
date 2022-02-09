5171f11999d5a488429ac832bb0fe0a6d5197d32
이재만 <lbefull@naver.com>
2022-02-10 00:27:13 +0900

    언팔로우 기능 추가

--- back-end/src/repositories/account.repository.ts
+++ back-end/src/repositories/account.repository.ts
@@ -40,13 +40,35 @@ export async function addFollow(targetId: string, myId: string) {
 
   try {
     result = await session.run(
-      `MATCH (me) - [rel:FOLLOW] ->(target) WHERE id(me) = ${myId} AND id(target) = ${targetId} RETURN rel`,
+      `MATCH (me) - [rel:FOLLOW] -> (target) WHERE id(me) = ${myId} AND id(target) = ${targetId} RETURN rel`,
     );
     if (result.records[0]) {
       return false;
     }
     await session.run(
       `MATCH (me), (target) WHERE id(me) = ${myId} AND id(target) = ${targetId} CREATE (me) - [:FOLLOW] -> (target)`,
+    );
+  } finally {
+    await session.close();
+  }
+
+  await driver().close();
+  return true;
+}
+
+export async function deleteFollow(targetId: string, myId: string) {
+  let result;
+  const session = driver().session();
+
+  try {
+    result = await session.run(
+      `MATCH (me) - [rel:FOLLOW] -> (target) WHERE id(me) = ${myId} AND id(target) = ${targetId} RETURN rel`,
+    );
+    if (!result.records[0]) {
+      return false;
+    }
+    await session.run(
+      `MATCH (me) - [rel:FOLLOW] -> (target) WHERE id(me) = ${myId} AND id(target) = ${targetId} DELETE rel`,
     );
   } finally {
     await session.close();
