3d9f9e69a11117dcc975aeec4c64f8214001af62
김석원 <seokworking1609@gmail.com>
2022-02-10 15:10:37 +0900

    방 가중치 정렬 스켈레톤생성(주석처리되어있음)39번째줄

--- back-end/src/services/room.service.ts
+++ back-end/src/services/room.service.ts
@@ -31,8 +31,29 @@ export class RoomService {
 
   public getRooms(): Room[] {
     // console.log('rooms : ', this.ROOMS);
+    //정렬로직 추가
+    //좋아한느 카테고리수 + 팔로우한 사람 수
     return this.ROOMS;
   }
+  /*
+  public sortRooms(): Room[] {
+    //방들을 관심사, 나랑친한 참여자에 따라 정렬해주는로직!! NEO4J와 user의 팔로잉 기준으로 정렬!!!그리고 카테고리도!!
+    const sortRooms: Room[]; // 나중에 리턴할 룸 목록
+    //const userFollowing: string[]; //유저 팔로잉 목록을 받음
+    this.ROOMS.forEach((room) => {
+      let weight: number = 0;
+      userFollowing.forEach((user) => {
+        if (room.participates.find(parti === user) != undefined) {
+          weight++;
+        }
+      }// end user for loop);
+      )
+    }//end room loop
+     //
+    
+    return sortRooms;
+}
+*/
 
   public getRoom(id: string): Room {
     const roomIndex = this.ROOMS.findIndex((room) => room.id === id);
