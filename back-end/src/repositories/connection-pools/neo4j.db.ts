807161bc4db3f0f0c53aa1279a133cbe6d433d98
Noah <23338687+enjay27@users.noreply.github.com>
2022-02-11 11:19:43 +0900

    stringify 메서드 구현

--- back-end/src/repositories/connection-pools/neo4j.db.ts
+++ back-end/src/repositories/connection-pools/neo4j.db.ts
@@ -3,3 +3,24 @@ import neo4j from 'neo4j-driver';
 export function driver() {
   return neo4j.driver(uri, neo4j.auth.basic(user, password));
 }
+
+/**
+ * 사용 example  
+ * 
+    const stringified = stringify(userDto);
+    result = await session.run(
+        `create (user:User ${stringified}) RETURN user`,
+    );
+ * @returns 
+ */
+export function stringify(obj: object): string {
+  let str = '{';
+
+  for (const key of Object.keys(obj)) {
+    str += `${key}:'${obj[key]}',`;
+  }
+  str = str.slice(0, -1);
+  str += '}';
+  console.log(str);
+  return str;
+}
