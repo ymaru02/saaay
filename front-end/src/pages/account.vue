570f3429f382f0c458d32f331c0b8a10ce5cd338
이재만 <lbefull@naver.com>
2022-02-11 11:56:06 +0900

    bugfix

--- front-end/src/pages/account.vue
+++ front-end/src/pages/account.vue
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */ /* eslint-disable
+@typescript-eslint/no-unsafe-assignment */
 <template>
   <div class="q-pa-md">
     <div class="row">
@@ -20,52 +22,66 @@
 
           <q-tab-panels v-model="tab" animated>
             <q-tab-panel name="followers">
-              <div class="row">
-                <div class="col-1 row">
-                  <div class="col-8 offset-2">
-                    <img
-                      src="images/blank-profile-picture.png"
-                      alt="profile-image"
-                      class="profile"
-                    />
+              <div
+                v-for="(follower, index) in followers"
+                :key="`follower-${index}`"
+              >
+                <div class="row">
+                  <div class="col-1 row">
+                    <div class="col-8 offset-2">
+                      <img
+                        src="images/blank-profile-picture.png"
+                        alt="profile-image"
+                        class="profile"
+                      />
+                    </div>
                   </div>
-                </div>
-                <div class="col-9 q-pl-lg row">
-                  <div class="col-12 row flex items-end">
-                    <div class="text-h5">Name</div>
-                    <div class="q-ml-lg">Follows You</div>
+                  <div class="col-9 q-pl-lg row">
+                    <div class="col-12 row flex items-end">
+                      <div class="text-h5">
+                        {{ follower._fields[0].properties.email }}
+                      </div>
+                      <div class="q-ml-lg">Follows You</div>
+                    </div>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
                   </div>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </p>
-                </div>
-                <div class="col-2 flex flex-center">
-                  <q-btn unelevated rounded color="primary" label="FOLLOW" />
+                  <div class="col-2 flex flex-center">
+                    <q-btn unelevated rounded color="primary" label="FOLLOW" />
+                  </div>
                 </div>
               </div>
             </q-tab-panel>
 
             <q-tab-panel name="following">
-              <div class="row">
-                <div class="col-1 row">
-                  <div class="col-8 offset-2">
-                    <img
-                      src="images/blank-profile-picture.png"
-                      alt="profile-image"
-                      class="profile"
-                    />
+              <div
+                v-for="(following, index) in followings"
+                :key="`following-${index}`"
+              >
+                <div class="row">
+                  <div class="col-1 row">
+                    <div class="col-8 offset-2">
+                      <img
+                        src="images/blank-profile-picture.png"
+                        alt="profile-image"
+                        class="profile"
+                      />
+                    </div>
                   </div>
-                </div>
-                <div class="col-9 q-pl-lg row">
-                  <div class="col-12 row flex items-end">
-                    <div class="text-h5">Name</div>
+                  <div class="col-9 q-pl-lg row">
+                    <div class="col-12 row flex items-end">
+                      <div class="text-h5">
+                        {{ following._fields[0].properties.email }}
+                      </div>
+                    </div>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
                   </div>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </p>
-                </div>
-                <div class="col-2 flex flex-center">
-                  <q-btn outline rounded color="primary" label="FOLLOWING" />
+                  <div class="col-2 flex flex-center">
+                    <q-btn outline rounded color="primary" label="FOLLOWING" />
+                  </div>
                 </div>
               </div>
             </q-tab-panel>
@@ -78,12 +94,42 @@
 
 <script>
 import { ref } from 'vue';
+import axios from 'axios';
 
 export default {
   setup() {
     return {
       tab: ref('followers'),
     };
+  },
+  data() {
+    return {
+      followers: [],
+      followings: [],
+    };
+  },
+  created() {
+    const targetId: string = this.$route.params.targetId;
+    axios
+      .get('http://localhost:3000/accounts/4/follower')
+      .then((result) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        this.followers = result.data;
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        console.log(this.$route.params.targetId);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    axios
+      .get('http://localhost:3000/accounts/4/following')
+      .then((result) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        this.followings = result.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
 };
 </script>
