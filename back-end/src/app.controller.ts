bda38be83abadb16d0ce6ba1d62c623368260557
Noah <23338687+enjay27@users.noreply.github.com>
2022-02-14 02:22:14 +0900

    jwt 적용

--- back-end/src/app.controller.ts
+++ back-end/src/app.controller.ts
@@ -12,17 +12,26 @@ import { SampleService } from './services/sample.service';
 import { TodoDto } from './models/todo.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly sampleService: SampleService,
+    private readonly authService: AuthService,
   ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 
