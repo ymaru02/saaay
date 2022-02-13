d8cae6e72e15e76ba8f2306c92a6d74528f10a9f
dyun022 <dyun022@gmail.com>
2022-02-13 14:50:53 +0900

    back merge

--- back-end/src/repositories/connection-pools/neo4j.db.ts
+++ back-end/src/repositories/connection-pools/neo4j.db.ts
@@ -3,3 +3,39 @@ import neo4j from 'neo4j-driver';
 export function driver() {
   return neo4j.driver(uri, neo4j.auth.basic(user, password));
 }
+
+/**
+ * 사용 example  
+ * 
+    const stringified = stringify(userDto);
+    result = await session.run(
+        `create (user:User ${stringified}) RETURN user`,
+    );
+ * @returns 
+ */
+export function stringify(obj: object): string {
+  let str = '{';
+
+  for (const key of Object.keys(obj)) {
+    str += `${key}:'${obj[key]}',`;
+  }
+  str = str.slice(0, -1);
+  str += '}';
+  console.log(str);
+  return str;
+}
+
+export async function executeQuery<T>(query: string): Promise<T> {
+  let result;
+  const session = driver().session();
+
+  try {
+    result = await session.run(query);
+  } finally {
+    await session.close();
+  }
+
+  await driver().close();
+
+  return result.records;
+}
--- back-end/src/services/account.service.ts
+++ back-end/src/services/account.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { getFollowerList } from '../repositories/account.repository';
+import {
+  getFollowerList,
+  getFollowingList,
+  addFollow,
+  deleteFollow,
+  addBlock,
+  deleteBlock,
+} from '../repositories/account.repository';
 
 @Injectable()
 export class AccountService {
@@ -7,101 +14,29 @@ export class AccountService {
     const result = await getFollowerList(targetId);
     return result;
   }
+
+  public async getFollowingList(targetId: string) {
+    const result = await getFollowingList(targetId);
+    return result;
+  }
+
+  public async addMyFollowingList(targetId: string, myId: string) {
+    const result = await addFollow(targetId, myId);
+    return result;
+  }
+
+  public async deleteMyFollowingList(targetId: string, myId: string) {
+    const result = await deleteFollow(targetId, myId);
+    return result;
+  }
+
+  public async addMyBlockList(targetId: string, myId: string) {
+    const result = await addBlock(targetId, myId);
+    return result;
+  }
+
+  public async deleteMyBlockList(targetId: string, myId: string) {
+    const result = await deleteBlock(targetId, myId);
+    return result;
+  }
 }
-
-// import db_config from '../config/db-config.json';
-
-// function test(): { selectedDate: string } {
-//   return {
-//     selectedDate: '123',
-//   };
-// }
-// export class Accounts {
-//   neo4j = require('neo4j-driver');
-//   uri = db_config.uri;
-//   user = db_config.user;
-//   password = db_config.password;
-
-//   public async getFollowerList(targetId: string) {
-//     const driver = this.neo4j.driver(
-//       this.uri,
-//       this.neo4j.auth.basic(this.user, this.password),
-//     );
-//     const session = driver.session();
-
-//     let result;
-
-//     try {
-//       result = await session.run(
-//         `MATCH (me) <- [:FOLLOW] - (target) WHERE id(me) = ${targetId} RETURN target`,
-//       );
-//       const followerList = [];
-
-//       for (const record of result.records) {
-//         followerList.push(record._fields[0]);
-//       }
-//     } finally {
-//       await session.close();
-//     }
-
-//     await driver.close();
-
-//     // return result.records;
-
-//     return {
-//       selectedDate: 11,
-//     };
-//   }
-
-//   public async getFollowingList(targetId: string) {
-//     const driver = this.neo4j.driver(
-//       this.uri,
-//       this.neo4j.auth.basic(this.user, this.password),
-//     );
-//     const session = driver.session();
-
-//     let result;
-
-//     try {
-//       result = await session.run(
-//         `MATCH (me) - [:FOLLOW] -> (target) WHERE id(me) = ${targetId} RETURN target`,
-//       );
-//       const followerList = [];
-
-//       for (const record of result.records) {
-//         followerList.push(record._fields[0]);
-//       }
-//     } finally {
-//       await session.close();
-//     }
-
-//     await driver.close();
-
-//     return result.records;
-//   }
-
-//   public async addMyFollowerList(targetId: string, myId: string) {
-//     const driver = this.neo4j.driver(
-//       this.uri,
-//       this.neo4j.auth.basic(this.user, this.password),
-//     );
-//     const session = driver.session();
-
-//     try {
-//       let result = await session.run(
-//         `MATCH (me) - [rel:FOLLOW] ->(target) WHERE id(me) = ${myId} AND id(target) = ${targetId} RETURN rel`,
-//       );
-//       if (result.records[0]) {
-//         return false;
-//       }
-//       await session.run(
-//         `MATCH (me), (target) WHERE id(me) = ${myId} AND id(target) = ${targetId} CREATE (me) - [:FOLLOW] -> (target)`,
-//       );
-//     } finally {
-//       await session.close();
-//     }
-
-//     await driver.close();
-//     return true;
-//   }
-// }
