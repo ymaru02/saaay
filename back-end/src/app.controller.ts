4238d9529089678dc6268a9fc148c62009cca10f
Noah <23338687+enjay27@users.noreply.github.com>
2022-02-13 16:07:21 +0900

    example 코드 적용

--- back-end/src/app.controller.ts
+++ back-end/src/app.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { SampleService } from './services/sample.service';
 import { TodoDto } from './models/todo.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -9,6 +19,12 @@ export class AppController {
     private readonly appService: AppService,
     private readonly sampleService: SampleService,
   ) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
 
   @Get('/sample')
   getSample(): TodoDto[] {
