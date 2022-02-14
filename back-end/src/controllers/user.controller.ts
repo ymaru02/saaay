b7d35be4d3b93d6102ec69b3d28e856718c24d0a
Noah <23338687+enjay27@users.noreply.github.com>
2022-02-15 01:27:58 +0900

    axios access token cookie 에 저장

--- back-end/src/controllers/user.controller.ts
+++ back-end/src/controllers/user.controller.ts
@@ -30,7 +30,7 @@ export class UserController {
     private readonly authService: AuthService,
   ) {}
 
-  @Get('/:userName')
+  @Get('/name/:userName')
   async searchUsername(
     @Param('userName') userName: string,
     @Res() res: Response,
@@ -91,7 +91,7 @@ export class UserController {
     }
   }
 
-  @Patch('/user/password')
+  @Patch('/password')
   @UseGuards(JwtAuthGuard)
   async editPassword(@Body() userDto: UserDto, @Res() res: Response) {
     console.log('edit password :', userDto);
@@ -103,11 +103,11 @@ export class UserController {
     }
   }
 
-  @Get('sample/verify')
+  @Get('/verify')
   async verify(@Headers('Authorization') accessToken, @Res() res: Response) {
     console.log(accessToken);
     const user = await this.authService.verifyUser(accessToken);
     console.log(user.email);
-    res.send();
+    if (user) res.status(HttpStatus.OK).json({ message: '' });
   }
 }
