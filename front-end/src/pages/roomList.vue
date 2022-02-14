8265615cd6d388b0fea4e917448056b314eae9bb
김석원 <seokworking1609@gmail.com>
2022-02-14 16:53:41 +0900

    "room page back 연동 성공"

--- front-end/src/pages/roomList.vue
+++ front-end/src/pages/roomList.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="q-pa-md row items-start q-gutter-md">
-    <q-card class="my-card">
-      <q-card-section class="bg-primary text-white">
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
-      </q-card-section>
+    <div v-for="(room, index) in rooms" :key="index">
+      <q-card class="my-card">
+        <q-card-section class="bg-primary text-white">
+          <div class="text-h6">{{ room.roomName }}</div>
+          <div class="text-subtitle2">by {{ room.moderator[0] }}</div>
+        </q-card-section>
 
-      <q-separator />
+        <q-separator />
 
-      <q-card-actions align="right">
-        <q-btn flat>Action 1</q-btn>
-        <q-btn flat>Action 2</q-btn>
-      </q-card-actions>
-    </q-card>
+        <q-card-actions align="right">
+          <q-btn flat>Action 1</q-btn>
+          <q-btn flat>Action 2</q-btn>
+        </q-card-actions>
+      </q-card>
+    </div>
 
     <q-card class="my-card">
       <q-card-section class="bg-purple text-white">
@@ -52,4 +54,27 @@
   </div>
 </template>
 
-<script src=""></script>
+<script lang="ts">
+import { computed, ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { useStore } from 'src/store';
+
+export default {
+  setup() {
+    // const route = useRoute();
+    // const router = useRouter();
+    const $store = useStore();
+
+    $store.dispatch('room/getRoomList').catch(console.log);
+
+    const loadRoomList = () => $store.dispatch('room/getRoomList');
+
+    const rooms = computed(() => $store.state.room.rooms);
+
+    return {
+      rooms,
+      loadRoomList,
+    }; //end setup
+  }, // end export default
+};
+</script>
--- front-end/src/store/index.ts
+++ front-end/src/store/index.ts
@@ -10,6 +10,8 @@ import {
 // import { ExampleStateInterface } from './module-example/state';
 import account from './module-account';
 import { AccountStateInterface } from './module-account/state';
+import room from './module-room';
+import { RoomStateInterface } from './module-room/state';
 
 /*
  * If not building with SSR mode, you can
@@ -26,6 +28,7 @@ export interface StateInterface {
   // Declared as unknown to avoid linting issue. Best to strongly type as per the line above.
   example: unknown;
   account: AccountStateInterface;
+  room: RoomStateInterface;
 }
 
 // provide typings for `this.$store`
@@ -44,6 +47,7 @@ export default store(function (/* { ssrContext } */) {
     modules: {
       // example
       account,
+      room,
     },
 
     // enable strict mode (adds overhead!)
