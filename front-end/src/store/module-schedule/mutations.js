faee01aaa30415abb85a8bef7ad4ee43eff6f7b8
kcw0360 <kcw0360@naver.com>
2022-02-16 19:55:21 +0900

    title로 ud로 변경

--- front-end/src/store/module-schedule/mutations.js
+++ front-end/src/store/module-schedule/mutations.js
@@ -1,4 +1,11 @@
+import { keyBy } from "lodash";
+
 export function GET_EVENT(state, save_event) {
-  console.log(save_event);
+  console.log("mut", save_event);
   state.events = save_event;
+}
+
+export function CREATE_EVENT(state, event) {
+  console.log(event);
+  state.events = event;
 }
